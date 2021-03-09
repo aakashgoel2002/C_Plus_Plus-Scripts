d906fb79e36b31b6dc2b1a0ee90497377ddc98ba
Aakash Goel <aakashgoel2002@gmail.com>
2021-03-09 22:31:41 +0530

    Update SumUsingFunction.cpp

--- SumUsingFunction.cpp
+++ SumUsingFunction.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+using namespace std;
+int sum(int a,int b){
+    return (a+b);
+}
+int main(){
+    int a,b;
+    cout<<"Enter 1st Number: ";
+    cin>>a;
+    cout<<"Enter 2nd Number: ";
+    cin>>b;
+    cout<<"The Sum of "<<a<<" and "<<b<<": "<<sum(a,b);
+    return 0;
+}
