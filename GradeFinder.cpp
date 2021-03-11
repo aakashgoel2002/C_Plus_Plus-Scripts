87a6c889689837285d1dc1484168aaa7d1259c80
Aakash Goel <aakashgoel2002@gmail.com>
2021-03-11 23:13:44 +0530

    Update GradeFinder.cpp

--- GradeFinder.cpp
+++ GradeFinder.cpp
@@ -1,1 +1,27 @@
-
+#include<iostream>
+using namespace std;
+int result(int marks){
+    if(marks>=91 && marks<=100)
+    cout<<"AA";
+    else if(marks>=81 && marks<=90)
+    cout<<"AB";
+    else if(marks>=71 && marks<=80)
+    cout<<"BB";
+    else if(marks>=61 && marks<=70)
+    cout<<"BC";
+    else if(marks>=51 && marks<=60)
+    cout<<"CD";
+    else if(marks>=41 && marks<=50)
+    cout<<"DD";
+    else if(marks>=0 && marks<=40)
+    cout<<"Fail";
+    else
+    cout<<"Invalid Input!";
+}
+int main(){
+    int marks;
+    cout<<"Enter Marks: ";
+    cin>>marks;
+    result(marks);
+    return 0;
+}
