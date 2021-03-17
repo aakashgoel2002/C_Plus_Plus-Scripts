8d147e1a9c9efeddb7c0d0b56abcf68bf951c3fd
Aakash Goel <aakashgoel2002@gmail.com>
2021-03-17 23:54:09 +0530

    Update LargestSmallestNumber.cpp

--- LargestSmallestNumber.cpp
+++ LargestSmallestNumber.cpp
@@ -1,1 +1,19 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int large=0,i,small=2147483647;
+    cout<<"Enter Number of Array Elements to be entered: ";
+    cin>>i;
+    int arr[i];
+    cout<<"Enter Array Elements: "<<endl;
+    for(int j=0;j<i;j++){
+        cin>>arr[j];
+        if(large<arr[j])
+        large=arr[j];
+        if(small>arr[j])
+        small=arr[j];
+    }
+    cout<<"Largest Number: "<<large<<endl;
+    cout<<"Smallest Number: "<<small<<endl;
+    return 0;
+}
